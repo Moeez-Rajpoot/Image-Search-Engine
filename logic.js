e97f847c84465d946f78f9d95bcbf536dec69fcc
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-17 18:52:42 +0500

    Updated

--- logic.js
+++ logic.js
@@ -1,1 +1,33 @@
-let 
+const input = document.getElementById('search-box');
+const searchbtn = document.getElementById('Search-btn');
+const displaydiv = document.getElementById('display');
+const show = document.getElementById('more');
+
+
+const accesskey = "Ye231JmnrvKeWozHoXo8RToWM51zD2MevZHfTT0qRfI";
+let page = 1;
+let keyword = "";
+
+
+
+
+async function getimages(){
+
+
+    let url =  `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}`;
+    let response = await fetch(url);
+    let data = response.json();
+
+    console.log(data);
+}
+
+
+searchbtn.addEventListener('click' ,(e)=>{
+
+
+    page=1;
+    keyword = input.value;
+    getimages();
+    
+
+})
